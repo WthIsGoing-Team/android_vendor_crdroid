29e5479b668b2161b009d7f1b8e9945208fc0383
zxcwsurx <viitiv.m@gmail.com>
2024-08-13 21:06:33 +0300

    вендар ыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыы

---
+++ config/common_full.mk
@@ -0,0 +1,27 @@
+# Inherit common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_mobile.mk)
+
+PRODUCT_SIZE := full
+
+# Apps
+PRODUCT_PACKAGES += \
+    Eleven \
+    Etar \
+    Profiles \
+    Recorder \
+    Seedvault
+
+ifneq ($(PRODUCT_NO_CAMERA),true)
+PRODUCT_PACKAGES += \
+    Aperture
+endif
+
+ifneq ($(TARGET_EXCLUDES_AUDIOFX),true)
+PRODUCT_PACKAGES += \
+    AudioFX
+endif
+
+# Extra cmdline tools
+PRODUCT_PACKAGES += \
+    unrar \
+    zstd
